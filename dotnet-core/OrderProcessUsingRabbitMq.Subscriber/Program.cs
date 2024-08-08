f6ce2073a9f3e67fdd2795824dfe560695661d65
Zeeshan Rais <developerxone@hotmail.com>
2024-08-08 17:23:28 +0300

    added consumer / subscriber console application to consume message from publisher

---
+++ dotnet-core/OrderProcessUsingRabbitMq.Subscriber/Program.cs
@@ -0,0 +1,25 @@
+﻿using RabbitMQ.Client;
+using RabbitMQ.Client.Events;
+using System.Text;
+
+var factory = new ConnectionFactory
+{
+    HostName = "localhost"
+};
+
+var connection = factory.CreateConnection();
+
+var channel = connection.CreateModel();
+channel.QueueDeclare("orders", exclusive: false);
+
+var consumer = new EventingBasicConsumer(channel);
+
+consumer.Received += (sender, args) =>
+{
+    var body = args.Body.ToArray();
+    var message = Encoding.UTF8.GetString(body);
+
+    Console.WriteLine($"Message received: {message}");
+};
+
+Console.ReadKey();
