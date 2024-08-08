b5dc98c7130efd1dddac1220147d50a0609e4af0
Zeeshan Rais <developerxone@hotmail.com>
2024-08-08 16:16:17 +0300

    Added new project in dotnet-core to explore RabbitMQ

---
+++ dotnet-core/OrderProcessUsingRabbitMq/Dtos/OrderDto.cs
@@ -0,0 +1,10 @@
+﻿namespace OrderProcessUsingRabbitMq.Dtos;
+
+public class OrderDto
+{
+    public required string ProductName { get; set; }
+
+    public double Price { get; set; }
+
+    public int Quantity { get; set; }
+}
---
+++ dotnet-core/OrderProcessUsingRabbitMq/Service/OrderService.cs
@@ -0,0 +1,29 @@
+﻿using OrderProcessUsingRabbitMq.Data;
+using OrderProcessUsingRabbitMq.Dtos;
+
+namespace OrderProcessUsingRabbitMq.Service;
+
+public class OrderService
+{
+    private readonly OrderDBContext orderDBContext;
+
+    public OrderService(OrderDBContext orderDBContext)
+    {
+        this.orderDBContext = orderDBContext;
+    }
+
+    public async Task<Order> Save(OrderDto orderDto)
+    {
+        Order order = new Order
+        {
+            ProductName = orderDto.ProductName,
+            Quantity = orderDto.Quantity,
+            Price = orderDto.Price
+        };
+
+        orderDBContext.Orders.Add(order);
+        order.Id  = await orderDBContext.SaveChangesAsync();
+
+        return order;
+    }
+}
