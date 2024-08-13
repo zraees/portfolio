2969a87897de6d213ac893b8fc43dc7882d181de
Zeeshan Rais <developerxone@hotmail.com>
2024-08-13 19:34:04 +0300

    added producer, consumer, and model projects to implement MassTransit using RabbitMq

---
+++ dotnet-core/OrderProcessUsingMassTransitRabbitMq/OrderProcessUsingMassTransitRabbitMq.SharedModels/IOrderCreated.cs
@@ -0,0 +1,9 @@
+﻿namespace OrderProcessUsingMassTransitRabbitMq.SharedModels;
+
+public interface IOrderCreated
+{
+    public int Id { get; set; }
+    public string? ProductName { get; set; }
+    public int Price { get; set; }
+    public int Quantity { get; set; }
+}
